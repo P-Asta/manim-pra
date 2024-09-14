f6ad3f5fd0366ab275e2676859de7fd3cf525585
5-23 <yhanbyeol6@gmail.com>
2024-09-14 16:01:05 +0900

    feat: vg

--- sus.py
+++ sus.py
@@ -2,8 +2,58 @@ from manim import *
 
 class Test(Scene):
     def construct(self):
-        let = Text("Integer String Boolean Array", font="Monospace")
-        self.play(Write(let))
+        zero = Text("Integer", font="monospace")
+        self.play(Write(zero))
+        one = VGroup(
+            VGroup(
+                heightlighting("i", "8"),
+                heightlighting("i", "16"),
+                heightlighting("i", "32"),
+                heightlighting("i", "64"),
+                heightlighting("i", "128"),
+                heightlighting("i", "size"),
+            ).arrange(RIGHT, buff=.3),
+
+
+            VGroup(
+                heightlighting("u", "8"),
+                heightlighting("u", "16"),
+                heightlighting("u", "32"),
+                heightlighting("u", "64"),
+                heightlighting("u", "128"),
+                heightlighting("u", "size"),
+            ).arrange(RIGHT, buff=.3)
+        )
+        one.arrange(DOWN, buff=.5)
+
+        self.play(ReplacementTransform(zero, one))
         self.wait(1)
-        lett = Text("Integer", font="Monospace")
-        self.play(ReplacementTransform(let, lett))
+        two = VGroup(
+            VGroup(
+                heightlighting("i", "8", color=BLUE),
+                heightlighting("i", "16", color=BLUE),
+                heightlighting("i", "32", color=BLUE),
+                heightlighting("i", "64", color=BLUE),
+                heightlighting("i", "128", color=BLUE),
+                heightlighting("i", "size"),
+            ).arrange(RIGHT, buff=.3),
+
+
+            VGroup(
+                heightlighting("u", "8", color=BLUE),
+                heightlighting("u", "16", color=BLUE),
+                heightlighting("u", "32", color=BLUE),
+                heightlighting("u", "64", color=BLUE),
+                heightlighting("u", "128", color=BLUE),
+                heightlighting("u", "size"),
+            ).arrange(RIGHT, buff=.3)
+        )
+        two.arrange(DOWN, buff=.5)
+        self.play(ReplacementTransform(one, two))
+
+        self.wait(1)
+
+def heightlighting(text, heightlighting, color = color.WHITE) -> VGroup:
+    vg = VGroup(Text(text, font="monospace"), Text(heightlighting, color=color, font="monospace"))
+    vg.arrange(RIGHT, buff=0.1)
+    return vg
